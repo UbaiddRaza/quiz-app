21f141a57a2dc0fe5131a780556806e1b2580e50
UbaiddRaza <ubaiddraza@gmail.com>
2024-10-18 11:51:25 +0500

    quiz app answers

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+const [question, setquestion] = useState(null)
+const [currentIndex, setcurrentIndex] = useState(0)
+  useEffect(() => {
+    axios('https://the-trivia-api.com/v2/questions')
+      .then((res) => {
+        console.log(res.data);
+        setquestion(res.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
+
+  const nextQuestion = ()=>{
+    if (currentIndex < question.length -1){
+      setcurrentIndex(currentIndex + 1)
+      return
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Quiz</h1>
+     {question ? 
+     <div><h1>Q{currentIndex + 1}: {question[currentIndex].question.text}</h1>
+     {question[currentIndex].incorrectAnswers.map((item, index)=>{
+    return <div key={'option${index}'}>
+      <input type="radio" value={item} name ='question' id={index}/>
+        <label htmlFor={index}>{item}</label>
+       
+     </div>
+     })}
+      <input type="radio" value={question[currentIndex].correctAnswer} name ='question' id='q'/>
+        <label htmlFor='q'>{question[currentIndex].correctAnswer}</label>
+       <br />
+       <br />
+     <button onClick={nextQuestion}>Next</button>
+
+     </div> : <h1>Loading...</h1>}
+   
     </>
-  )
+   
+  );
 }
 
-export default App
+export default App;
